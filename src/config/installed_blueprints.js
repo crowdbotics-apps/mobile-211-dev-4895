1cca5a40ed6d61801c30760868fddff816b322cf
Crowdbotics <team@crowdbotics.com>
2020-05-21 07:11:29 +0000

    remove package blueprint-react-native-profiles-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,6 +1,5 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
-{ name: 'UserProfile4428', human_name: 'User Profile', access_route: 'UserProfile4428'},
 { name: 'EmailAuth', human_name: 'EmailAuth', access_route: 'EmailAuth', icon: 'envelope-o'},
 
   // you can add more installed blueprints here
